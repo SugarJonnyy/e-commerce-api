98eee4bfb6c30ae943f68dac190eae4da2e46b6c
Saswata Das <220040079+SugarJonnyy@users.noreply.github.com>
2026-05-25 19:51:36 +0530

    config dotenv, port & connect to db

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('E-Commerce API');
+require('dotenv').config()
+
+//express
+const express = require('express')
+
+//database
+const connectDB = require('./db/connect')
+const app = express()
+
+
+const port = process.env.PORT || 5000
+const start = async ()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, ()=> console.log(`server is listening to the ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+start()
